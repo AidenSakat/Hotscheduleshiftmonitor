45ea78916f9c2aab01bc263ed0265454cf51b212
AidenSakat <126438824+AidenSakat@users.noreply.github.com>
2024-12-30 18:34:09 -0800

    Add files via upload

---
+++ HotschedulesShiftPickup/config.py
@@ -0,0 +1,14 @@
+# config.py
+# Login credentials
+USERNAME = "Aidensakat"
+PASSWORD = "Aiden123321"
+
+# Website URL
+URL = "https://app.hotschedules.com/hs/login.jsp"
+
+# Notification settings
+NOTIFICATION_TIMEOUT = 60  # Notification display timeout in seconds
+REFRESH_PAGE = 10 # How often to refresh page to check for shifts
+off_days = ["Tue" ,"Wed"]
+
+auto_pickup = True
---
+++ HotschedulesShiftPickup/main.py
@@ -0,0 +1,74 @@
+from imports import *
+from config import *
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+  
+driver = webdriver.Chrome(options=chrome_options)
+
+def login():
+    driver.get(URL)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loginusername"))).send_keys(USERNAME)
+    driver.find_element(By.ID, "loginpassword").send_keys(PASSWORD)
+    driver.find_element(By.ID, "loginBtn").click()
+
+# Login to the site
+login()
+
+# Wait until the "Available Shifts" button is available and click it
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "Available Shifts1"))
+).click()
+
+# Main loop to keep checking for shifts
+while True:
+    containers = driver.find_elements(By.CLASS_NAME, "future")
+
+    for container in containers:
+        try:
+            available_shifts = container.find_elements(By.CLASS_NAME, "shift-pickup-item")
+            current_day = container.find_element(By.CLASS_NAME, "day-name").text
+            shifts_count = len(available_shifts)
+
+            # check if shift is available / > 0
+            if shifts_count > 0:
+                container.find_element(By.CLASS_NAME, "item-shifts-amount").click()
+                
+                # Wait for shift time to appear
+                shift_time = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 
+                        ".shifts-table-body .table-item.shift-time"))
+                ).text
+                
+                # Send notification
+                if current_day not in off_days:
+                  notification.notify(
+                      title=f"{shifts_count} shifts found on {current_day}",
+                      message=f"Shift time: {shift_time}",
+                      timeout=NOTIFICATION_TIMEOUT
+                  )
+                  if auto_pickup:
+                    WebDriverWait(driver, 5).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, ".echo-component-EchoButton-2670g.echo-component-footer-2KDZu"))
+                    ).click()
+                    notification.notify(
+                      title=f"PICKED UP {current_day}",
+                      message=f"Shift time: {shift_time}",
+                      timeout=NOTIFICATION_TIMEOUT
+                  )
+
+                # Close the "pick up shift tab" (ESC)
+                webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+            time.sleep(1)  # Short delay before processing the next container
+
+        except Exception as e:
+            print(f"Error processing day: {e}")
+    
+    # Wait before refreshing the page and checking again
+    print("Waiting for the next check...")
+    time.sleep(REFRESH_PAGE)  # Adjust this to the frequency of your checks (60 seconds here)
+
+    # Refresh the page to get updated shifts
+    driver.refresh()
+    time.sleep(5)  # Wait for the page to reload before continuing
